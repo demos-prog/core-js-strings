055ecdc88a2674f87fed5df3019a50010a5ba20d
demos-prog <demosne@gmail.com>
2024-11-28 09:59:57 +0300

    4

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -251,8 +251,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const propMinutes = minutes.toString().padStart(2, '0');
+  const propSeconds = seconds.toString().padStart(2, '0');
+  return `${propMinutes}:${propSeconds}`;
 }
 
 /**
